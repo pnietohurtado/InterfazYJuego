92abc9784b64c1eabc6972df49080425c481e0b1
pnh0002 <pnietohurtado03@gmail.com>
2025-10-08 16:29:58 +0200

    Primer commit

---
+++ InterfazAvanzada/src/main/java/com/mycompany/interfazavanzada/InterfazAvanzada.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfazavanzada;
+
+/**
+ *
+ * @author pablo
+ */
+public class InterfazAvanzada {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
